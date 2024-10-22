a1c1dc18be8a6c3bae46614f8bb7789cb5061408
Piotr Sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2024-10-22 08:46:49 +0000

    Trochę poprawek

--- .ccls-cache/@home@runner@quicksort/main.cpp
+++ .ccls-cache/@home@runner@quicksort/main.cpp
@@ -2,11 +2,6 @@
 #include <vector>
 #include <string>
 
-
-bool less(float a, float b){
-    return a < b;
-}
-
 bool less(char a, char b){
     return a < b;
 }
@@ -15,33 +10,29 @@ bool less(std::string a, std::string b){
     return a.length() < b.length();
 }
 
-bool less(int a, int b){
-    return a * a < b * b;
-}
 
 
 template <typename T>
-int partition(std::vector<T>& tab, int lo) {
-    int hi = tab.size() - 1;
-    T pivot = tab[lo];  // Wybieramy element osiowy (pierwszy element)
+int partition(std::vector<T>& v, int lo, int hi) {
+    // Podział na a[lo..i-1], a[i], a[i+1..hi]
     int i = lo, j = hi + 1;  // Lewy i prawy wskaźnik
+    T pivot = v[lo];  // Wybieramy element osiowy (pierwszy element)
 
     while (true) {
         // Znajdź element większy od pivota z lewej strony
-        while (less(tab[++i], pivot)) {
-            if (i == hi) break;
-        }
+        while (less(v[++i], pivot)) if (i == hi) break;
+        
         // Znajdź element mniejszy od pivota z prawej strony
-        while (less(pivot, tab[--j])) {
-            if (j == lo) break;
-        }
+        while (less(pivot, v[--j])) if (j == lo) break;
+        
         // Jeśli wskaźniki się spotkają, przerwij
         if (i >= j) break;
+        
         // Zamień elementy, które są nie na swoim miejscu
-        std::swap(tab[i], tab[j]);
+        std::swap(v[i], v[j]);
     }
     // Zamień element osiowy na właściwe miejsce
-    std::swap(tab[lo], tab[j]);
+    std::swap(v[lo], v[j]);
 
     return j;  // Zwróć indeks pivota
 }
@@ -50,41 +41,47 @@ template <typename T>
 void sort(std::vector<T>& v, int lo, int hi)
 {
     if (hi <= lo) return;
-    int j = partition(v, lo); // Podział (zobacz stronę 303).
+    int j = partition(v, lo, hi); // Podział 
     sort(v, lo, j - 1); // Sortowanie lewej strony a[lo .. j-1].
     sort(v, j + 1, hi); // Sortowanie prawej strony a[j+1 .. hi].
 }
 
-
+template <typename T>
+void sort(std::vector<T>& v)
+{
+    sort(v, 0, v.size() - 1);
+}
 
 template <typename T>
 void print(const std::vector<T>& v) {    
     for (int i = 0; i < v.size(); i ++)
-        std::cout << v[i] << "\t";
+        std::cout << v[i] << "    ";
+
+    std::cout<<std::endl;
 }
 
 
 
 int main() 
 {
-    std::vector<char> v_char = {'K', 'B', 'D', 'L', 'W', 'I', 'G', 'S', 'Q', 'L', 'X', 'C'};
-    print(v_char);
+    // std::vector<char> v_char = {'K', 'B', 'D', 'L', 'W', 'I', 'G', 'S', 'Q', 'L', 'X', 'C'};
+    // print(v_char);
+    // partition(v_char, 0);
+    // sort(v_char, 0, v_char.size() - 1);
+    // print(v_char);
 
-    std::cout<<std::endl;
-    
-    partition(v_char, 0);
-    print(v_char);
-    sort(v_char, 0, v_char.size() - 1);
-
-
-    // std::vector<std::string> v_str = {"POLSKA", "HOLANDIA", "JEMEN", "UL", "USA", "SEMINARIUM", "JABŁKO"};
-    // print(v_str);
     // std::cout<<std::endl;
 
+    // std::vector<std::string> v_str = {"SEMINARIUM", "JEDEN", "GRUSZKA", "POLSKA", "INFORMATYKA", "UL", "C++"};
+    // print(v_str);
     // partition(v_str, 0);
+    // sort(v_str, 0, v_str.size() - 1);
     // print(v_str);
 
-    // sort(v_str, 0, v_str.size() - 1);
-    // std::cout<<std::endl;
-    // print(v_str);
+    std::vector<int> v_int = {7, 5, 2, 1, 6, 9, 8, 4};
+    print(v_int);
+    sort(v_int);
+    print(v_int);
+
+    return 0;
 }
